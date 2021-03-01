86b30121c7b9ca3c8ec2c49d9b5ded6e54288bbe
tumiemosweu <imosweuk@gmail.com>
2021-03-01 16:44:37 +0200

    add search slug

--- timesheet/models/monthly_entry.py
+++ timesheet/models/monthly_entry.py
@@ -1,6 +1,7 @@
 from edc_base.model_mixins import BaseUuidModel
 from edc_base.model_validators import date_not_future
 from edc_base.sites.site_model_mixin import SiteModelMixin
+from edc_search.model_mixins import SearchSlugModelMixin
 from django.db.models.deletion import PROTECT
 from django.db import models
 from django.core.validators import MinValueValidator
@@ -9,15 +10,13 @@ from bhp_personnel.models import Employee, Supervisor
 
 from ..choices import ENTRY_TYPE, STATUS
 
-class MonthlyEntry(SiteModelMixin, BaseUuidModel):
+class MonthlyEntry(SiteModelMixin, SearchSlugModelMixin, BaseUuidModel):
 
     employee = models.ForeignKey(Employee, on_delete=PROTECT)
 
     supervisor = models.ForeignKey(Supervisor, on_delete=PROTECT)
 
-    month = models.DateField(
-#                              validators=[date_not_future, ],
-                            )
+    month = models.DateField()
 
     comment = models.TextField(
          max_length=100,
@@ -39,6 +38,12 @@ class MonthlyEntry(SiteModelMixin, BaseUuidModel):
             total_hours += h.duration
         return total_hours
 
+    def get_search_slug_fields(self):
+        fields = super().get_search_slug_fields()
+        fields.append('employee')
+        fields.append('supervisor')
+        return fields
+
     class Meta:
         app_label = 'timesheet'
         unique_together = ('month', 'employee')
@@ -47,9 +52,7 @@ class DailyEntry(BaseUuidModel):
 
     monthly_entry = models.ForeignKey(MonthlyEntry, on_delete=PROTECT)
 
-    day = models.DateField(
-#                            validators=[date_not_future, ]
-                            )
+    day = models.DateField()
 
     duration = models.IntegerField(
         validators=[MinValueValidator(0)])
