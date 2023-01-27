33a581466bce74713e1f605f0c48be5c0f532710
salahdin <salahdinjemal@gmail.com>
2023-01-27 15:43:39 +0200

    additional signal for HR

--- timesheet/models/__init__.py
+++ timesheet/models/__init__.py
@@ -1,2 +1,2 @@
 from .monthly_entry import MonthlyEntry, DailyEntry
-from .signals import timesheet_on_post_save
+from .signals import timesheet_on_post_save, timesheet_on_status_update
--- timesheet/models/signals.py
+++ timesheet/models/signals.py
@@ -38,3 +38,44 @@ def timesheet_on_post_save(sender, instance, raw, created, **kwargs):
                 msg.send()
             except Exception as e:
                 raise
+
+
+@receiver(post_save, weak=False, sender=MonthlyEntry,
+          dispatch_uid='timesheet_on_post_save')
+def timesheet_on_status_update(sender, instance, raw, created, **kwargs):
+    # triggered when status changes
+    if instance and not created:
+        if instance.status == 'approved':
+            employee = instance.employee
+            site_url = f"https://{get_current_site(request=None).domain}"
+            hr_email = "dthebe@bhp.org.bw"
+            from_email = "bhp.se.dmc@gmail.com"
+            subject = "Timesheet submission"
+
+            body = f"""\
+                        Dear HR,
+                        <br>
+                        <br>
+                        This is an automated notification to inform you that  {instance.approved_by},
+                        has approved the { instance.month } timesheet for {employee.employee_code} 
+                        (Employee ID: {employee.employee_code}). 
+                        <br>
+                        Please log in to the system to verify it.
+                        <br>
+                        <br>
+                        <a href="{site_url}" target="_blank">Visit Site</a>
+                        <br>
+                        <br>
+                        Thank you,
+                        """
+            msg = EmailMultiAlternatives(subject=subject, body=body,
+                                         from_email=from_email, to=(hr_email,))
+            msg.content_subtype = 'html'
+            print("Sending to : ", hr_email)
+            try:
+                msg.send()
+            except Exception as e:
+                raise
+
+
+
