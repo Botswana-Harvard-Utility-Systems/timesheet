896c654f8a85632bdf328a181eff6c478f5fb0d5
salahdin <salahdinjemal@gmail.com>
2023-01-30 09:32:47 +0200

    refactor code

--- timesheet/models/signals.py
+++ timesheet/models/signals.py
@@ -6,19 +6,27 @@ from django.dispatch import receiver
 from timesheet.models import MonthlyEntry
 
 
-@receiver(post_save, weak=False, sender=MonthlyEntry,
-          dispatch_uid='timesheet_on_post_save')
-def timesheet_on_post_save(sender, instance, raw, created, **kwargs):
+def send_email(to, subject, body, from_email='bhp.se.dmc@gmail.com'):
+    msg = EmailMultiAlternatives(subject=subject, body=body, from_email=from_email, to=(to,))
+    msg.content_subtype = 'html'
+    print(f"Sending to: {to}")
+    try:
+        msg.send()
+    except Exception as e:
+        raise
+
+
+@receiver(post_save, weak=False, sender=MonthlyEntry, dispatch_uid='timesheet_on_post_save')
+def timesheet_notification(sender, instance, raw, created, **kwargs):
+    employee = instance.employee
+    site_url = f"https://{get_current_site(request=None).domain}"
+    supervisor = employee.supervisor
     if not raw:
         if created:
-            employee = instance.employee
-            supervisor = employee.supervisor
-            site_url = f"https://{get_current_site(request=None).domain}"
-            from_email = "bhp.se.dmc@gmail.com"
             subject = "Timesheet submission"
-
+            recipient = supervisor.email
             body = f"""\
-                         Dear {supervisor.first_name} {supervisor.last_name},
+                        Dear {supervisor.first_name} {supervisor.last_name},
                         <br>
                         <br>
                         This is an automated notification to inform you that {employee.first_name} (Employee ID: {employee.employee_code}),
@@ -30,34 +38,16 @@ def timesheet_on_post_save(sender, instance, raw, created, **kwargs):
                         <br>
                         Thank you,
                         """
-            msg = EmailMultiAlternatives(subject=subject, body=body,
-                                         from_email=from_email, to=(supervisor.email,) )
-            msg.content_subtype = 'html'
-            print("Sending to : ", supervisor.email)
-            try:
-                msg.send()
-            except Exception as e:
-                raise
-
-
-@receiver(post_save, weak=False, sender=MonthlyEntry,
-          dispatch_uid='timesheet_on_post_save')
-def timesheet_on_status_update(sender, instance, raw, created, **kwargs):
-    # triggered when status changes
-    if instance and not created:
-        if instance.status == 'approved':
-            employee = instance.employee
-            site_url = f"https://{get_current_site(request=None).domain}"
-            hr_email = "dthebe@bhp.org.bw"
-            from_email = "bhp.se.dmc@gmail.com"
-            subject = "Timesheet submission"
-
+            send_email(recipient, subject, body)
+        elif not created and instance.status == 'approved':
+            subject = "Timesheet Approval"
+            recipient = "dthebe@bhp.org.bw"
             body = f"""\
                         Dear HR,
                         <br>
                         <br>
                         This is an automated notification to inform you that  {instance.approved_by},
-                        has approved the { instance.month } timesheet for {employee.employee_code} 
+                        has approved the {instance.month} timesheet for {employee.employee_code} 
                         (Employee ID: {employee.employee_code}). 
                         <br>
                         Please log in to the system to verify it.
@@ -68,14 +58,4 @@ def timesheet_on_status_update(sender, instance, raw, created, **kwargs):
                         <br>
                         Thank you,
                         """
-            msg = EmailMultiAlternatives(subject=subject, body=body,
-                                         from_email=from_email, to=(hr_email,))
-            msg.content_subtype = 'html'
-            print("Sending to : ", hr_email)
-            try:
-                msg.send()
-            except Exception as e:
-                raise
-
-
-
+            send_email(recipient, subject, body)
